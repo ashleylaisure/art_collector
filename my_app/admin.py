820a85b118e93dcc307a1d06ce65d10eb7cff1ea
Ashley Laisure <ashley.laisure@gmail.com>
2025-05-28 15:15:09 -0400

    start of one to many relationships

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Art
+from .models import Art, Viewing
 # Register your models here.
 admin.site.register(Art)
+admin.site.register(Viewing)
--- my_app/models.py
+++ my_app/models.py
@@ -1,44 +1,41 @@
 from django.db import models
 from django.urls import reverse
 
+medium_choices = (
+    ('oil', 'Oil Paint'),
+    ('acrylic', 'Acrylic Paint'),
+    ('watercolor', 'Watercolor'),
+    ('gouache', 'Gouache'),
+    ('pastels', 'Pastels'),
+    ('ink', 'Ink'),
+    ('graphite', 'Graphite/Charcoal'),
+    ('color_pencils', 'Colored Pencils'),
+    ('mixed_media', 'Mixed Media'),
+)
+    
+movement_choices = (
+    ('abstract', 'Abstract Expressionism'),
+    ('art_deco', 'Art Deco'),
+    ('art_nouveau', 'Art Nouveau'),
+    ('baroque', 'Baroque'),
+    ('classicism', 'Classicism'),
+    ('contemporary', 'Contemporary Art'),
+    ('cubism','Cubism'),
+    ('expressionism', 'Expressionism'),
+    ('realism', 'Hyperrealism'),
+    ('renaissance', 'Renaissance'),
+    ('impressionism', 'Impressionism'),
+    ('modern', 'Modern Art'),
+    ('neoclassicism', 'Neoclassicism'),
+    ('pop', 'Pop Art'),
+    ('rococo', 'Rococo'),
+    ('surrealism', 'Surrealism'),
+)
 # Create your models here.
 class Art(models.Model):
-    medium_choices = [
-        ('oil', 'Oil Paint'),
-        ('acrylic', 'Acrylic Paint'),
-        ('watercolor', 'Watercolor'),
-        ('gouache', 'Gouache'),
-        ('pastels', 'Pastels'),
-        ('ink', 'Ink'),
-        ('graphite', 'Graphite/Charcoal'),
-        ('color_pencils', 'Colored Pencils'),
-        ('mixed_media', 'Mixed Media'),
-        ('sculpture', 'Sculpture'),
-        ('architecture', 'Architecture'),
-    ]
-    
-    movement_choices = [
-        ('abstract', 'Abstract Expressionism'),
-        ('art_deco', 'Art Deco'),
-        ('art_nouveau', 'Art Nouveau'),
-        ('baroque', 'Baroque'),
-        ('classicism', 'Classicism'),
-        ('contemporary', 'Contemporary Art'),
-        ('cubism','Cubism'),
-        ('expressionism', 'Expressionism'),
-        ('realism', 'Hyperrealism'),
-        ('impressionism', 'Impressionism'),
-        ('modern', 'Modern Art'),
-        ('neoclassicism', 'Neoclassicism'),
-        ('pop', 'Pop Art'),
-        ('rococo', 'Rococo'),
-        ('surrealism', 'Surrealism'),
-    ]
-    
-    
     title = models.CharField(max_length=100)
     artist = models.CharField(max_length=100)
-    date = models.CharField(max_length=50)
+    date = models.CharField('Year Completed', max_length=50)
     medium = models.CharField(max_length=100, choices=medium_choices)
     movement = models.CharField(max_length=100, choices=movement_choices)
     image = models.URLField(max_length=200)
@@ -51,3 +48,12 @@ class Art(models.Model):
         # Use the 'reverse' function to dynamically find the URL for viewing this models's details
         return reverse("art-detail", kwargs={'art_id': self.id})
     
+    
+class Viewing(models.Model):
+    date = models.DateField()
+    museum = models.CharField(max_length=200)
+    
+    art = models.ForeignKey(Art, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.museum} on {self.date}"
